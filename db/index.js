bfa0d7306375df3cfb20fd47eb5a04ea4e06b72c
Yosa Rama Dinata <yosaramadinata@gmail.com>
2021-07-19 18:32:50 +0800

    Fix db connection

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ import mysql from "serverless-mysql";
 
 export const db = mysql({
   config: {
-    host: process.env.MYSQL_HOST,
+    host: process.env.MYSQL_ENDPOINT,
     database: process.env.MYSQL_DATABASE,
     port: +process.env.MYSQL_PORT,
     user: process.env.MYSQL_USERNAME,
