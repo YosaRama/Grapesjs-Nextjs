1e0097f6eb959ff3141157adaca338f3fedcded7
Yosa Rama Dinata <yosaramadinata@gmail.com>
2021-09-14 09:43:19 +0800

    Test deploy upload to local version

--- pages/api/media/upload.js
+++ pages/api/media/upload.js
@@ -26,21 +26,21 @@ const upload = multer({
 const apiRoute = nextConnect();
 
 //* Upload single file
-// apiRoute.post(upload.single("theFiles"), async (req, res) => {
-//   const file = req.file;
-//   const fileName = file.filename;
-//   const fileType = file.mimetype;
-//   const filePath = file.path.replace("public", "");
-//   try {
-//     const result = await Query(
-//       "INSERT INTO media (dimension, filename, mimetype, url) VALUES (?,?,?,?)",
-//       ["main", fileName, fileType, filePath]
-//     );
-//     res.status(200).json({ data: "Success save file" });
-//   } catch (error) {
-//     throw new Error(error);
-//   }
-// });
+apiRoute.post(upload.single("theFiles"), async (req, res) => {
+  const file = req.file;
+  const fileName = file.filename;
+  const fileType = file.mimetype;
+  const filePath = file.path.replace("public", "");
+  try {
+    // const result = await Query(
+    //   "INSERT INTO media (dimension, filename, mimetype, url) VALUES (?,?,?,?)",
+    //   ["main", fileName, fileType, filePath]
+    // );
+    res.status(200).json({ data: "Success save file" });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 //* Upload Multiple file with client side handle
 // apiRoute.post(upload.array("theFiles"), async (req, res) => {
@@ -90,100 +90,100 @@ const apiRoute = nextConnect();
 // });
 
 //* Upload file with server side
-apiRoute.post(upload.single("theFiles"), async (req, res) => {
-  const file = req.file;
-  const fileName = file.filename;
-  const fileType = file.mimetype;
-  const filePath = file.path.replace("public", "");
-  const fileDest = file.destination.substr(8, file.destination.length);
+// apiRoute.post(upload.single("theFiles"), async (req, res) => {
+//   const file = req.file;
+//   const fileName = file.filename;
+//   const fileType = file.mimetype;
+//   const filePath = file.path.replace("public", "");
+//   const fileDest = file.destination.substr(8, file.destination.length);
 
-  try {
-    const result = await Query(
-      "INSERT INTO media (dimension, filename, mimetype, url) VALUES (?,?,?,?)",
-      ["main", fileName, fileType, filePath]
-    );
-    if (result) {
-      try {
-        // Resize to 1920px
-        await sharp(req.file.path)
-          .resize(1920)
-          .toFile(
-            path.resolve(req.file.destination, "1920px@" + fileName),
-            async (err, info) => {
-              try {
-                const parentId = await Query(
-                  "SELECT id FROM media WHERE filename=?",
-                  [fileName]
-                );
-                const currentId = await parentId[0]?.id;
-                const thumbUrl = await (fileDest + "/1920px@" + fileName);
+//   try {
+//     const result = await Query(
+//       "INSERT INTO media (dimension, filename, mimetype, url) VALUES (?,?,?,?)",
+//       ["main", fileName, fileType, filePath]
+//     );
+//     if (result) {
+//       try {
+//         // Resize to 1920px
+//         await sharp(req.file.path)
+//           .resize(1920)
+//           .toFile(
+//             path.resolve(req.file.destination, "1920px@" + fileName),
+//             async (err, info) => {
+//               try {
+//                 const parentId = await Query(
+//                   "SELECT id FROM media WHERE filename=?",
+//                   [fileName]
+//                 );
+//                 const currentId = await parentId[0]?.id;
+//                 const thumbUrl = await (fileDest + "/1920px@" + fileName);
 
-                const result = await Query(
-                  "INSERT INTO media (parent_id, dimension, filename, mimetype, url) VALUES (?,?,?,?,?)",
-                  [currentId, "large", fileName, fileType, thumbUrl]
-                );
-              } catch (e) {
-                console.log(e);
-              }
-            }
-          );
-        // Resize to 720px
-        await sharp(req.file.path)
-          .resize(720)
-          .toFile(
-            path.resolve(req.file.destination, "720px@" + fileName),
-            async (err, info) => {
-              try {
-                const parentId = await Query(
-                  "SELECT id FROM media WHERE filename=?",
-                  [fileName]
-                );
-                const currentId = await parentId[0]?.id;
-                const thumbUrl = await (fileDest + "/720px@" + fileName);
+//                 const result = await Query(
+//                   "INSERT INTO media (parent_id, dimension, filename, mimetype, url) VALUES (?,?,?,?,?)",
+//                   [currentId, "large", fileName, fileType, thumbUrl]
+//                 );
+//               } catch (e) {
+//                 console.log(e);
+//               }
+//             }
+//           );
+//         // Resize to 720px
+//         await sharp(req.file.path)
+//           .resize(720)
+//           .toFile(
+//             path.resolve(req.file.destination, "720px@" + fileName),
+//             async (err, info) => {
+//               try {
+//                 const parentId = await Query(
+//                   "SELECT id FROM media WHERE filename=?",
+//                   [fileName]
+//                 );
+//                 const currentId = await parentId[0]?.id;
+//                 const thumbUrl = await (fileDest + "/720px@" + fileName);
 
-                const result = await Query(
-                  "INSERT INTO media (parent_id, dimension, filename, mimetype, url) VALUES (?,?,?,?,?)",
-                  [currentId, "medium", fileName, fileType, thumbUrl]
-                );
-                if (result) {
-                  res.status(200).send({ message: "Success upload file" });
-                }
-              } catch (e) {
-                console.log(e);
-              }
-            }
-          );
-        // Resize to 150px
-        await sharp(req.file.path)
-          .resize(150)
-          .toFile(
-            path.resolve(req.file.destination, "150px@" + fileName),
-            async (err, info) => {
-              try {
-                const parentId = await Query(
-                  "SELECT id FROM media WHERE filename=?",
-                  [fileName]
-                );
-                const currentId = await parentId[0]?.id;
-                const thumbUrl = await (fileDest + "/150px@" + fileName);
+//                 const result = await Query(
+//                   "INSERT INTO media (parent_id, dimension, filename, mimetype, url) VALUES (?,?,?,?,?)",
+//                   [currentId, "medium", fileName, fileType, thumbUrl]
+//                 );
+//                 if (result) {
+//                   res.status(200).send({ message: "Success upload file" });
+//                 }
+//               } catch (e) {
+//                 console.log(e);
+//               }
+//             }
+//           );
+//         // Resize to 150px
+//         await sharp(req.file.path)
+//           .resize(150)
+//           .toFile(
+//             path.resolve(req.file.destination, "150px@" + fileName),
+//             async (err, info) => {
+//               try {
+//                 const parentId = await Query(
+//                   "SELECT id FROM media WHERE filename=?",
+//                   [fileName]
+//                 );
+//                 const currentId = await parentId[0]?.id;
+//                 const thumbUrl = await (fileDest + "/150px@" + fileName);
 
-                const result = await Query(
-                  "INSERT INTO media (parent_id, dimension, filename, mimetype, url) VALUES (?,?,?,?,?)",
-                  [currentId, "small", fileName, fileType, thumbUrl]
-                );
-              } catch (e) {
-                console.log(e);
-              }
-            }
-          );
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  } catch (e) {
-    console.log(e);
-  }
-});
+//                 const result = await Query(
+//                   "INSERT INTO media (parent_id, dimension, filename, mimetype, url) VALUES (?,?,?,?,?)",
+//                   [currentId, "small", fileName, fileType, thumbUrl]
+//                 );
+//               } catch (e) {
+//                 console.log(e);
+//               }
+//             }
+//           );
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     }
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 
 export default apiRoute;
 
